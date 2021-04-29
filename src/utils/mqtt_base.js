13c7497ae2fbe3a16121281a4eaa7818c3727db6
hearsay316 <qazwsx2228@163>
2021-04-29 17:57:51 +0800

    登录完成 (开始封装 mqtt)

---
+++ src/utils/mqtt_base.js
@@ -0,0 +1,22 @@
+const mqttData = {
+  production: {
+    clientId: "stage",
+    username: "test",
+    password: "passwd",
+    url: "wss://www.huaitaos.com:8445"
+  },
+  staging: {
+    clientId: "stage",
+    username: "test",
+    password: "passwd",
+    url: "ws://58.213.48.218:12080/ws-stage/"
+  },
+  development: {
+    clientId: "stage",
+    username: "test",
+    password: "passwd",
+    url: "ws://58.213.48.218:12080/ws-stage/"
+  }
+};
+export default mqttData[process.env.VUE_APP_ENV];
+
