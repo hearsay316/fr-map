d1e1db7c214f0ac2aa0d0e689524d5824f6a5fe0
hearsay <hearsay316@users.noreply.github.com>
2021-05-13 17:43:16 +0800

    1 做了基础布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="home-container">
         <home-left-container class="left-container"></home-left-container>
-        <div class="right-container"></div>
+        <div class="right-container">
+            <div class="right-container-main">
+                <div class="home-history"></div>
+                <div class="home-record"></div>
+                <div class="battle-list"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,6 +32,37 @@ export default {
     background: url('../assets/page/home-left-bg.png');
 }
 .right-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #0d151b;
     //background: url("../assets/page/right.png");
+}
+.right-container-main {
+    position: relative;
+    width: column-width(3185);
+    height: column-width(1935);
+    border: 2px solid red;
+    overflow: hidden;
+}
+.battle-list {
+    //1590-1935
+    position: absolute;
+    width: column-width(1590);
+    height: column-width(1935);
+    top: 0;
+    right: 0;
+    border: 2px solid red;
+}
+.home-history {
+    width: column-width(1520);
+    height: column-width(645);
+    border: 1px solid red;
+    margin-bottom: column-width(70);
+}
+.home-record {
+    width: column-width(1680);
+    height: column-width(1220);
+    border: 1px solid red;
 }
 </style>
