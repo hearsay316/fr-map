de994ea8578f2f9c0ec8187345f8fcfceb5398f9
hearsay <hearsay316@users.noreply.github.com>
2021-05-17 15:59:50 +0800

    1 页面表格的布局

--- src/views/home-record.vue
+++ src/views/home-record.vue
@@ -68,9 +68,9 @@
             style="width: 100%"
             :row-class-name="tableRowClassName"
         >
-            <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-            <el-table-column prop="address" label="地址"> </el-table-column>
+            <el-table-column align="center" prop="date" label="日期" width="180"> </el-table-column>
+            <el-table-column align="center" prop="name" label="姓名" width="180"> </el-table-column>
+            <el-table-column align="center" prop="address" label="地址"> </el-table-column>
         </el-table>
     </div>
 </template>
@@ -112,8 +112,7 @@ const tableData = reactive([
         address: '上海市普陀区金沙江路 1518 弄'
     }
 ]);
-function tableRowClassName({ row, rowIndex }) {
-    console.log(6666);
+function tableRowClassName({ rowIndex }) {
     if (rowIndex % 2 === 1) {
         return 'warning-row';
     }
